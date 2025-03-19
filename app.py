3e97ab0da7e9580796adf94ae48ee87a02712c86
KRISHNENDU BIR <23EE01025@iitbbs.ac.in>
2025-03-19 21:52:10 +0530

    first commit

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def caculatededuction():
         return jsonify({"error": str(e)}), 500    
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    app.run(debug=False, host="0.0.0.0", port=8000)
